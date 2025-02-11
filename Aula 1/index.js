f4c50f2ff5012d997ecdc12866258715f4a9cf32
gugosabugo <liviasayurishirotargino@gmail.com>
2025-02-11 10:45:47 -0300

    Aula 1

---
+++ Aula 1/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send("Página principal")
+})
+
+app.get('/home', (req, res)=>{
+    res.send("Página home")
+})
+
+app.get('/login', (req, res)=>{
+    res.send("Página login")
+})
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log('Servidor rodando na porta', port)
+})
