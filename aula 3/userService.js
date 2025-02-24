60a778a9175b934f66cfa271216fa79dc81797e5
gugosabugo <liviasayurishirotargino@gmail.com>
2025-02-24 17:05:14 -0300

    aula 4

--- aula 3/userService.js
+++ aula 3/userService.js
@@ -1,9 +1,33 @@
-const User = require("./user")
+const User = require("user")
+const path = require("path") //modulo para manipular caminhos
+const fs = require("fs")// modulo para manipular arquivos file system
 
 class userService{
     constructor(){ //quando não passa parâmetro traz um valor fixo, que não muda
+        this.filePath = path.join(__dirname, 'user.json')
         this.users = [] //[] é um array, esse array é pra armazenar o user
         this.nextID = 1 //contador para gerar id
+    }
+
+    loadUsers(){
+        try{
+            if(fs.existsSync(this.filePath)){
+                const data = fs.readFileSync(this.filePath)
+                return JSON.parse(data)
+            }
+        }catch(erro){
+            console.log("Erro ao carregar arquivo", erro)
+        }
+        return [] //retorna um array vazio
+    }
+
+    getNextId(users){
+        try{
+        if(this.users.length === 0) return 1
+            return Math.max(...this.users.map(user => user.id))+1
+        }catch (erro){
+            console.log("Erro ao buscar próximo id", erro)
+        }
     }
     
     addUser(nome,email){
