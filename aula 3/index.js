2814a12c8e532a202ef44b3d9542f169522a924c
gugosabugo <liviasayurishirotargino@gmail.com>
2025-02-24 14:56:49 -0300

    versao certa aula 3, definitivamente

--- aula 3/index.js
+++ aula 3/index.js
@@ -1,29 +1,26 @@
-const express = require('express');
-const userService = require('./userService')
+const express = require('express')
+const userService = require ('./userservice')
 
-const app = express()
-app.use(express.json); //Vou habilitar json no express
+const app = express() //nome pro express, pode ser qualquer coisa
+app.use(express.json()) //ativa o json no express
 
-//rota para criar usuário
 
-app.post('/users', (req,res) =>{
-    const {nome, email} = req.body;
-    if(!nome || !email){
-        return res.status(400).json
-        ({error: "Nome e Email são dados obrigatórios"})
-    }
-
-    const user = userService.addUser(nome, email);
-    res.status(200).json({user});
+//rota para usuario ser criado
+app.post("/users", (req, res) =>{
+    const {nome, email} = req.body //passa um arquivo via json pra nome e email
+     if(!nome || !email){ //caso o nome e o email sejam diferentes de (estejam vazios) vai dar erro
+        return res.status(400).json ({error: "Nome e email são obrigatórios"}) //mensagem enviada caso dê erro (nome ou email vazios)
+     }
+     const user = userService.addUser(nome, email)
+     res.status(200).json({user})
 })
 
-//rota para mostrar os usuários
+//rota pra listar todos os usuarios
+app.get("/users", (req, res)=>{
+    res.json(userService.getUsers())
+})
 
-app.get("/users", (req, res) =>{
-    res.json(userService.getUsers)
-});
-
-const port = 3000;
-app.listen(port, ()=>{
-    console.log("Servidor rodando na porta", port);
+const port = 3000
+app.listen (port, () =>{
+    console.log("O servidor está rodando na porta: ", port)
 })
--- aula 3/user.js
+++ aula 3/user.js
@@ -1,15 +1,8 @@
-class User{
-    constructor(id,nome,email){
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
+class User {
+    constructor(id, nome, email){
+        this.id = id
+        this.nome = nome
+        this.email = email
     }
 }
-class Admin extends User{
-    constructor(id,nome,email,nivelAcesso){
-        super(id,nome,email)
-        this.nivelAcesso = nivelAcesso
-    }
-}
-
-module.export = User //Exporta a classe User
+module.exports = User //vai exportar o user 
